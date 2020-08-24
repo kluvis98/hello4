acb1e1ba2f7141da6fb5ee30ef0d12736c81ce22
Isaiah Kluver <isaiah.kluver@huskers.unl.edu>
2020-08-24 04:32:13 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,14 +1,15 @@
 /**
  * Author: Isaiah Kluver
  * Date: 2020/8/23
- * 
+ *
  * Hack 1
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     printf("Isaiah Kluver!\n");
+     printf("Isaiah Joshuah Kluver!\n");
+     printf("Psychology and Economics!\n");
      return 0;
-     
+
  }
