854842d3d7243e5018ec3464cca7739448a7dd07
Isaiah Kluver <isaiah.kluver@huskers.unl.edu>
2020-08-24 04:09:13 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Isaiah Kluver
+ * Date: 2020/8/23
+ * 
+ * Hack 1
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     printf("Isaiah Kluver!\n");
+     return 0;
+     
+ }
